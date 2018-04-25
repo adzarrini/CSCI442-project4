b2e41f8d5ad7ad77411b105a11760700d3bf95ba
Allee Zarrini <adzarrini@bb136-12.mines.edu>
2018-04-24 22:03:15 -0600

    Passed process tests

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -11,29 +11,38 @@ using namespace std;
 
 Process* Process::read_from_input(std::istream& in) {
   // TODO: implement me
-  return nullptr;
+  size_t num_bytes = 0;
+  Page* page;
+  vector<Page*> pages;
+
+  while((page = Page::read_from_input(in)) != nullptr) {
+    pages.push_back(page);
+    num_bytes+= page->size();
+  }
+  return new Process(num_bytes, pages);
 }
 
 
 size_t Process::size() const {
   // TODO: implement me
-  return 0;
+  return num_bytes;
 }
 
 
 bool Process::is_valid_page(size_t index) const {
   // TODO: implement me
-  return false;
+  return pages[index]!=nullptr;
 }
 
 
 size_t Process::get_rss() const {
   // TODO: implement me
-  return 0;
+  return page_table.get_present_page_count();
 }
 
 
 double Process::get_fault_percent() const {
   // TODO: implement me
-  return 0.0;
+  if(memory_accesses == 0) return 0.0;
+  return 100.0*double(page_faults)/memory_accesses;
 }
