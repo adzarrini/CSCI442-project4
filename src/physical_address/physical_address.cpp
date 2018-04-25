86dfddf187e5458ce69b3d13453d71cf8e74d2c0
Allee Zarrini <adzarrini@bb136-12.mines.edu>
2018-04-24 18:48:04 -0600

    Implemented PhysicalAddress tests.

--- src/physical_address/physical_address.cpp
+++ src/physical_address/physical_address.cpp
@@ -5,17 +5,24 @@
  */
 
 #include "physical_address/physical_address.h"
+#include <bitset>
 
 using namespace std;
 
 
 string PhysicalAddress::to_string() const {
   // TODO: implement me
-  return "";
+	bitset<FRAME_BITS> frame_bits(frame);
+	bitset<OFFSET_BITS> offset_bits(offset); 
+  return frame_bits.to_string()+offset_bits.to_string();
 }
 
 
 ostream& operator <<(ostream& out, const PhysicalAddress& address) {
-  // TODO: implement me
-  return out;
+  // TODO: implement me'
+  return out
+      << address.to_string()
+      << " [frame: " << address.frame
+      << "; offset: " << address.offset
+      << "]";;
 }
