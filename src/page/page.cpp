e4ab7ec4a0e94545cb4b6c844bdb4b4b52e4d572
Allee Zarrini <adzarrini@bb136-12.mines.edu>
2018-04-24 20:51:02 -0600

    Passing tests for page

--- src/page/page.cpp
+++ src/page/page.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "page/page.h"
+#include <algorithm>
 
 using namespace std;
 
@@ -15,23 +16,32 @@ const size_t Page::PAGE_SIZE;
 
 Page* Page::read_from_input(std::istream& in) {
   // TODO: implement me
-  return nullptr;
+  if(in.peek()==-1) return nullptr;
+
+  vector<char> bytes;
+  size_t counter = 0;
+	while(counter++ < Page::PAGE_SIZE && in.peek() != -1) {
+		bytes.push_back(char(in.get()));
+	}
+
+  return new Page(bytes);
 }
 
 
 size_t Page::size() const {
   // TODO: implement me
-  return 0;
+  return (size_t) bytes.size();
 }
 
 
 bool Page::is_valid_offset(size_t offset) const {
   // TODO: implement me
+  if(offset < size()) return true;
   return false;
 }
 
 
 char Page::get_byte_at_offset(size_t offset) {
   // TODO: implement me
-  return 0;
+  return bytes[offset];
 }
